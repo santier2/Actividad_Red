cc77990bd46ac06f204df246765edfe0b14e0772
santier2 <131536136+santier2@users.noreply.github.com>
2025-08-27 09:21:18 -0300

    Comandos.py

--- Comandos.py
+++ Comandos.py
@@ -141,15 +141,17 @@ def run_check(device, cmds):
 # Ejecucion
 # --------------------------
 if __name__ == "__main__":
-    print(">>> Verifica ping a 10.10.12.1 .2 .3 .4 ANTES de correr este script <<<")
+    print(">>> Verificá ping a 10.10.12.1 .2 .3 .4 ANTES de correr este script <<<")
     time.sleep(2)
 
     run_cfg(r1, r1_phase1)              # R1 VLANs, DHCP, NAT
     run_cfg(sw1, sw1_phase_transitory)  # SW1 VLANs y trunk nativa 1299
     run_cfg(r2, r2_phase1)              # R2 tabla VLANs
+    run_cfg(sw2, sw2_phase_transitory)  # SW2 VLANs y trunk nativa 1299
+
+    # recién ahora cambiamos a 239
     run_cfg(r1, r1_phase2)              # R1 cambia pvid a 239
     run_cfg(sw1, sw1_phase_finalize)    # SW1 cambia native a 239
-    run_cfg(sw2, sw2_phase_transitory)  # SW2 VLANs y trunk nativa 1299
     run_cfg(sw2, sw2_finalize)          # SW2 native 239 (opcional)
 
     # Verificaciones
@@ -157,7 +159,6 @@ if __name__ == "__main__":
     run_check(sw2, ["show vlan brief","show interface trunk"])
     run_check(r1, ["/interface bridge vlan print","/ip address print","/ip dhcp-server print"])
     run_check(r2, ["/interface bridge vlan print","/ip address print"])
-
     # --------------------------
     # Resumen final
     # --------------------------
@@ -167,3 +168,4 @@ if __name__ == "__main__":
             print(f"{host:20} -> Configurado correctamente")
         else:
             print(f"{host:20} -> ERROR: {err}")
+
